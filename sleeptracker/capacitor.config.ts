afa45d8947694b0e581450466e5e6dad84360161
Daniel Epstein <epstein@ics.uci.edu>
2025-02-02 15:04:54 -0800

    Update for 2025

---
+++ sleeptracker/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sleeptracker',
+  webDir: 'www'
+};
+
+export default config;
--- sleeptracker/src/app/services/sleep.service.ts
+++ sleeptracker/src/app/services/sleep.service.ts
@@ -20,9 +20,22 @@ export class SleepService {
 	}
 
 	private addDefaultData() {
-		this.logOvernightData(new OvernightSleepData(new Date('February 18, 2021 01:03:00'), new Date('February 18, 2021 09:25:00')));
-		this.logSleepinessData(new StanfordSleepinessData(4, new Date('February 19, 2021 14:38:00')));
-		this.logOvernightData(new OvernightSleepData(new Date('February 20, 2021 23:11:00'), new Date('February 21, 2021 08:03:00')));
+		var goToBed = new Date();
+		goToBed.setDate(goToBed.getDate() - 1); //set to yesterday
+		goToBed.setHours(1, 3, 0); //1:03am
+		var wakeUp = new Date();
+		wakeUp.setTime(goToBed.getTime() + 8 * 60 * 60 * 1000); //Sleep for exactly eight hours, waking up at 9:03am
+		this.logOvernightData(new OvernightSleepData(goToBed, wakeUp)); // add that person was asleep 1am-9am yesterday
+		var sleepinessDate = new Date();
+		sleepinessDate.setDate(sleepinessDate.getDate() - 1); //set to yesterday
+		sleepinessDate.setHours(14, 38, 0); //2:38pm
+		this.logSleepinessData(new StanfordSleepinessData(4, sleepinessDate)); // add sleepiness at 2pm
+		goToBed = new Date();
+		goToBed.setDate(goToBed.getDate() - 1); //set to yesterday
+		goToBed.setHours(23, 11, 0); //11:11pm
+		wakeUp = new Date();
+		wakeUp.setTime(goToBed.getTime() + 9 * 60 * 60 * 1000); //Sleep for exactly nine hours
+		this.logOvernightData(new OvernightSleepData(goToBed, wakeUp));
 	}
 
 	public logOvernightData(sleepData:OvernightSleepData) {
